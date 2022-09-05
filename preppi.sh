91220c235eaaa993e1216dc46824aeeba2106a61
Gunars Frickauss <gunarsf@gmail.com>
2022-09-05 10:40:44 +0300

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -9,7 +9,6 @@ ln /opt/kiosk/firstboot.service /lib/systemd/system/firstboot.service
 ln /opt/kiosk/kiosk.ini /home/pi/kiosk.ini
 systemctl enable firstboot.service
 
-
 sed -i '/^# Additional overlays.*/a dtoverlay=pi3-disable-wifi\ndtoverlay=pi3-disable-bt' /boot/config.txt
 apt-get --yes --allow-downgrades --allow-remove-essential --allow-change-held-packages install cups cups-bsd libcups2-dev
 cupsctl --remote-admin --remote-any
