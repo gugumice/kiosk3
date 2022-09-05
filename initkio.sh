24a18ffd2200aa05bc9cf2e0196a36b213b54d2b
Gunars Frickauss <gunarsf@gmail.com>
2022-09-05 11:49:12 +0300

    Update initkio.sh

--- initkio.sh
+++ initkio.sh
@@ -6,7 +6,6 @@ fi
 systemctl enable kiosk.service
 systemctl disable firstboot.service
 raspi-config --expand-rootfs > /dev/null
-CURRENT_HOSTNAME=$(cat /proc/sys/kernel/hostname)
 sleep 5
 IPO=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 |  cut -d. -f2);
 NEW_HOSTNAME="rpi-kio-"$IPO
