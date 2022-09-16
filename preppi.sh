b5d2a34c7eeb829c2158d4f18ee7cc945c3b7c5f
Gunars Frickauss <gunarsf@gmail.com>
2022-09-16 11:47:50 +0300

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -3,7 +3,7 @@
 sudo raspi-config nonint do_memory_16
 systemctl disable bluetooth.service
 systemctl disable hciuart.service
-apt-get update && apt-get upgrade
+apt-get update && apt-get -y upgrade
 
 ln /opt/kiosk/kiosk.service /lib/systemd/system/kiosk.service
 ln /opt/kiosk/firstboot.service /lib/systemd/system/firstboot.service
